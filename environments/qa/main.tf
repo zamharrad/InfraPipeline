4472ee210a986005dfe259c453fc3f18583703ea
unknown <mhmdzamhar@gmail.com>
2025-12-27 17:15:58 +0530

    added Jenkinsfile and QA environment

---
+++ environments/qa/main.tf
@@ -0,0 +1,17 @@
+
+module "vpc" {
+  source = "../../modules/vpc"
+  vpc_cidr      = "10.0.0.0/16"
+  env           = "qa"
+  subnet_cidr   = "10.0.1.0/24"
+  subnet_az     = "${var.aws_region}a"  # Use the aws_region variable here ap-southeast-1a
+}
+
+module "ec2" {
+  source        = "../../modules/ec2"
+  env           = "qa"
+  ami_id        = "ami-05f071c65e32875a8"  # Example AMI ID, replace with a valid one for your region
+  instance_type = "t2.micro"
+  subnet_id     = module.vpc.subnet_id
+  ec2_count     = 1
+}
